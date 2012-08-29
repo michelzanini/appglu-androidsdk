688e5959c103122aa50607f9768f867c33f7af4f
Michel Zanini <michel.zanini@arctouch.com>
2012-08-29 09:59:49 -0300

    Improving Device hashCode()

--- appglu-java-client/src/main/java/com/appglu/Device.java
+++ appglu-java-client/src/main/java/com/appglu/Device.java
@@ -47,10 +47,7 @@ public class Device {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((token == null) ? 0 : token.hashCode());
-		return result;
+		return (token == null) ? 0 : token.hashCode();
 	}
 
 	@Override
