4332c83dd7c66f3bcdbba3f1bdcaac25c504be77
Michel Zanini <michel.zanini@arctouch.com>
2012-09-12 11:06:53 -0300

    Adding SavedQuery object, a new way of executing saved queries

--- appglu-android-sample/src/main/java/com/appglu/android/sample/MainActivity.java
+++ appglu-android-sample/src/main/java/com/appglu/android/sample/MainActivity.java
@@ -6,41 +6,73 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.appglu.AsyncCallback;
+import com.appglu.QueryResult;
 import com.appglu.Rows;
 import com.appglu.android.AppGlu;
+import com.appglu.android.SavedQuery;
 
 public class MainActivity extends Activity {
-
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main);
         
-        this.readAllQueries();
+        AppGlu.crudApi().readAllInBackground("appglu_queries", this.rowsCallback);
     }
-
-	private void readAllQueries() {
-		AppGlu.crudApi().readAllInBackground("appglu_queries", new AsyncCallback<Rows>() {
-			
-			private ProgressDialog progressDialog;
-			
-        	public void onPreExecute() {
-        		this.progressDialog = ProgressDialog.show(MainActivity.this, "Loading", "Please wait...");
-        	}
-        	
-			public void onResult(Rows rows) {
-				Toast.makeText(MainActivity.this, rows.toString(), Toast.LENGTH_LONG).show();
-			}
-			
-			public void onException(Exception exception) {
-				Toast.makeText(MainActivity.this, exception.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-			}
-			
-			public void onFinish() {
-				this.progressDialog.dismiss();
-			}
-			
-		});
-	}
     
+    protected void runQueryWithService() {
+    	AppGlu.savedQueriesApi().runQueryInBackground("queryName1", this.queryResultCallback);
+    }
+    
+    protected void runQueryWithObject() {
+    	SavedQuery query = new SavedQuery("queryName1");
+        query.runInBackground(this.queryResultCallback);
+    }
+    
+    private AsyncCallback<Rows> rowsCallback = new AsyncCallback<Rows>() {
+		
+		private ProgressDialog progressDialog;
+		
+    	public void onPreExecute() {
+    		this.progressDialog = ProgressDialog.show(MainActivity.this, "Loading", "Please wait...");
+    	}
+    	
+		public void onResult(Rows rows) {
+			Toast.makeText(MainActivity.this, rows.toString(), Toast.LENGTH_SHORT).show();
+		}
+		
+		public void onException(Exception exception) {
+			Toast.makeText(MainActivity.this, exception.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+		}
+		
+		public void onFinish() {
+			this.progressDialog.dismiss();
+			runQueryWithService();
+		}
+		
+	};
+	
+	private AsyncCallback<QueryResult> queryResultCallback = new AsyncCallback<QueryResult>() {
+		
+		private ProgressDialog progressDialog;
+		
+    	public void onPreExecute() {
+    		this.progressDialog = ProgressDialog.show(MainActivity.this, "Loading", "Please wait...");
+    	}
+    	
+		public void onResult(QueryResult queryResult) {
+			Toast.makeText(MainActivity.this, queryResult.toString(), Toast.LENGTH_SHORT).show();
+		}
+		
+		public void onException(Exception exception) {
+			Toast.makeText(MainActivity.this, exception.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+		}
+		
+		public void onFinish() {
+			this.progressDialog.dismiss();
+		}
+		
+	};
+	
 }
--- appglu-java-client/src/main/java/com/appglu/ErrorResponse.java
+++ appglu-java-client/src/main/java/com/appglu/ErrorResponse.java
@@ -1,6 +1,10 @@
 package com.appglu;
 
-public class ErrorResponse {
+import java.io.Serializable;
+
+public class ErrorResponse implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Error error;
 	
