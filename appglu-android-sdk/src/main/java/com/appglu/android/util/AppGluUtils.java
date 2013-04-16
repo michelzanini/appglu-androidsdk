9bb0fa1b1858998ae6eab0abd3b82017eacf6ede
Michel Zanini <michel.zanini@arctouch.com>
2013-04-16 18:52:44 -0300

    Adding support for calculating inSampleSize using a requested width and height when downloading Bitmaps in StorageApi

--- appglu-android-sdk/src/main/java/com/appglu/android/util/AppGluUtils.java
+++ appglu-android-sdk/src/main/java/com/appglu/android/util/AppGluUtils.java
@@ -23,6 +23,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.TypedValue;
 
 /**
  * Contains utility and helper methods.
@@ -76,7 +77,7 @@ public abstract class AppGluUtils {
 	}
 	
 	/**
-	 * Decodes an image File to to Bitmap without resizing the image.<br>
+	 * Decodes an image File to Bitmap without resizing the image.<br>
 	 * Be careful, if the image is big this can become a problem because it will use to much memory.
 	 * @param file the image file reference
 	 * @return the Bitmap
@@ -102,8 +103,8 @@ public abstract class AppGluUtils {
 	/**
 	 * Decodes an image File to Bitmap resizing the image to fit the <code>requestedWidth</code> and <code>requestedHeight</code> dimensions.
 	 * @param file the image file reference
-	 * @param requestedWidth the final image width will be close to the requestedWidth
-	 * @param requestedHeight the final image height will be close to the requestedHeight
+	 * @param requestedWidth the final image width will be close to the requestedWidth (value is in pixels)
+	 * @param requestedHeight the final image height will be close to the requestedHeight (value is in pixels)
 	 * @return the resized Bitmap
 	 */
 	public static Bitmap decodeSampledBitmapFromFile(File file, int requestedWidth, int requestedHeight) {
@@ -112,21 +113,66 @@ public abstract class AppGluUtils {
 	    
 	    BitmapFactory.decodeFile(file.getPath(), options);
 	    
-	    int inSampleSize = calculateInSampleSize(options, requestedWidth, requestedHeight);
+	    int inSampleSize = calculateSampleSize(options, requestedWidth, requestedHeight);
 	    
 	    return decodeSampledBitmapFromFile(file, inSampleSize);
 	}
 	
-	public static int calculateInSampleSize(BitmapFactory.Options options, int requestedWidth, int requestedHeight) {
+	/**
+	 * Decodes an image byte[] to Bitmap without resizing the image.<br>
+	 * Be careful, if the image is big this can become a problem because it will use to much memory.
+	 * @param imageBytes image content in byte[]
+	 * @return the Bitmap
+	 */
+	public static Bitmap decodeSampledBitmapFromByteArray(byte[] imageBytes) {
+		return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+	}
+	
+	/**
+	 * Decodes an image byte[] to Bitmap resizing the image to be <code>inSampleSize</code> times smaller then the original.
+	 * @param imageBytes image content in byte[]
+	 * @param inSampleSize how much smaller that the image will be, for example, <code>inSampleSize</code> equals 2 will return an image 1/2 the size of the original
+	 * @return the resized Bitmap
+	 */
+	public static Bitmap decodeSampledBitmapFromByteArray(byte[] imageBytes, int inSampleSize) {
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		
+	    options.inSampleSize = inSampleSize;
+	    
+	    return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length, options);
+	}
+	
+	/**
+	 * Decodes an image byte[] to Bitmap resizing the image to fit the <code>requestedWidth</code> and <code>requestedHeight</code> dimensions.
+	 * @param imageBytes image content in byte[]
+	 * @param requestedWidth the final image width will be close to the requestedWidth (value is in pixels)
+	 * @param requestedHeight the final image height will be close to the requestedHeight (value is in pixels)
+	 * @return the resized Bitmap
+	 */
+	public static Bitmap decodeSampledBitmapFromByteArray(byte[] imageBytes, int requestedWidth, int requestedHeight) {
+		BitmapFactory.Options options = new BitmapFactory.Options();
+	    options.inJustDecodeBounds = true;
+	    
+	    BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length, options);
+	    
+	    int inSampleSize = calculateSampleSize(options, requestedWidth, requestedHeight);
+	    
+	    return decodeSampledBitmapFromByteArray(imageBytes, inSampleSize);
+	}
+	
+	/**
+	 * Giving a max width and height in pixels this method will calculate a good value to use in BitmapFactory.Options.inSampleSize.
+	 */
+	public static int calculateSampleSize(BitmapFactory.Options options, int requestedWidth, int requestedHeight) {
 	    // Raw height and width of image
-	    final int height = options.outHeight;
-	    final int width = options.outWidth;
+	    int width = options.outWidth;
+	    int height = options.outHeight;
 	    int inSampleSize = 1;
 	
-	    if (height > requestedHeight || width > requestedWidth) {
+	    if (width > requestedWidth || height > requestedHeight) {
 	        // Calculate ratios of height and width to requested height and width
-	        final int heightRatio = Math.round((float) height / (float) requestedHeight);
 	        final int widthRatio = Math.round((float) width / (float) requestedWidth);
+	        final int heightRatio = Math.round((float) height / (float) requestedHeight);
 	
 	        // Choose the smallest ratio as inSampleSize value, this will guarantee
 	        // a final image with both dimensions larger than or equal to the
@@ -136,5 +182,13 @@ public abstract class AppGluUtils {
 	
 	    return inSampleSize;
 	}
+	
+	/**
+	 * Given a value in DIP (density per pixel) uses the display metrics of the device to return the value in pixels.
+	 */
+	public static int valueInDipToPixels(Context context, float valueInDip) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                valueInDip, context.getResources().getDisplayMetrics());
+    }
 	
 }
