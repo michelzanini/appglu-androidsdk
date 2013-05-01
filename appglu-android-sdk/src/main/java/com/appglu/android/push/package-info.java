e0848778902a4b370d8049f04acb4c4620dddf02
Michel Zanini <michel.zanini@arctouch.com>
2013-05-01 10:28:39 -0300

    Adding missing package JavaDocs

---
+++ appglu-android-sdk/src/main/java/com/appglu/android/push/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains the Push API implementation for Android.<br>
+ * The Push API is used to register / unregister the device making it eligible to receive push notifications.<br>
+ * 
+ * @see com.appglu.android.push.PushApi
+ * @since 1.0.0
+ */
+package com.appglu.android.push;
